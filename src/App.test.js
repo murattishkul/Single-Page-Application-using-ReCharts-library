f9bcea1564bba651de8e407e689e5a9f2f52235f
Murat Tishkul <murat.tishkul@gmail.com>
2019-06-17 21:00:28 +0900

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import LineTotal from './index.js';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<LineTotal />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
